9ea874f7eb0f893106236b9ee635e5325dbe9051
Mark <dorianmj@gmail.com>
2018-09-04 22:49:51 +0300

    Init

---
+++ app.ts
@@ -0,0 +1,95 @@
+declare function require( name:string );
+
+let axios 	= require( 'axios' ),
+	async 	= require( 'async' ),
+	fs		= require( 'fs' ),
+	process = require( 'process' ),
+	md5		= require( 'md5' ),
+	jsdom 	= require( 'jsdom' ),
+	{ JSDOM } = jsdom;
+
+let proxies = {},
+	proxies_good = [];
+
+let lists = [ {
+	cb: ( callback ) => {
+		let proxylists = [
+			'https://free-proxy-list.net/',
+			'https://www.us-proxy.org/',
+			'https://free-proxy-list.net/anonymous-proxy.html'
+		];
+		async.eachOfLimit( proxylists, 3, ( proxylist, i, callback ) => {
+			axios( {
+				url: proxylist
+			} ).then( response => {
+				let data = response.data,
+					{ document } = ( new JSDOM( data ) ).window,
+					dataTable = document.getElementById( 'proxylisttable' ),
+					trs = dataTable.querySelectorAll( 'tbody tr' );
+				trs.forEach( tr => {
+					let tds 	= Array.prototype.slice.call( tr.querySelectorAll( 'td' ) ),
+						ip 		= tds[ 0 ].textContent.trim(),
+						port 	= tds[ 1 ].textContent.trim(),
+						key		= md5( ip );
+					if ( ! ( key in proxies ) ) {
+						proxies[ key ] = {
+							proxy: ip + ':' + port,
+							ip: ip,
+							port: port,
+							rate: 0
+						};
+					}
+				} );
+				
+				callback();
+			} ).catch( error => {
+				console.log( error );
+				callback();
+			} );
+		}, error => {
+			callback();
+		} );
+	}
+} ];
+
+console.log( 'Init' );
+
+let worker = ( restart: boolean = false ) => {
+	if ( restart ) console.log( 'Restart' );
+	async.eachOfLimit( lists, 5, ( list, i, callback ) => {
+		list.cb( callback );
+	}, error => {
+		console.log( 'Checking' );
+		async.eachOfLimit( proxies, 50, ( proxy, i, callback ) => {
+			console.log( 'Checking proxy ' + proxy[ 'proxy' ] );
+			axios( {
+				url: 'http://markschk.ru/',
+				timeout: 5000,
+				proxy: {
+					host: proxy[ 'ip' ],
+					port: proxy[ 'port' ]
+				}
+			} ).then( response => {
+				let data = response.data;
+				if ( data.includes( 'Mark Shkliaruk' ) ) {
+					proxies[ i ][ 'rate' ]++;
+				} else {
+					proxies[ i ][ 'rate' ]--;
+				}
+				console.log( 'Checking proxy ' + proxy[ 'proxy' ] + ' done' );
+				callback();
+			} ).catch( error => {
+				proxies[ i ][ 'rate' ] -= 10;
+				console.log( error );
+				callback();
+			} );
+		}, error => {
+			debugger;
+			setTimeout( () => {
+				worker( true );
+			}, 1000 );
+		} );
+	} );
+};
+
+worker();
